bdcea9451ccdbcd5122edd933224cb394dbc6fb1
ChoiAnYong <dksdyd78@naver.com>
2023-12-28 23:12:27 +0900

    채팅 기능 구현

---
+++ LMessenger/Repository/DTO/ChatObject.swift
@@ -0,0 +1,26 @@
+//
+//  ChatObject.swift
+//  LMessenger
+//
+//  Created by 최안용 on 12/28/23.
+//
+
+import Foundation
+
+struct ChatObject: Codable {
+    var chatId: String
+    var userId: String
+    var message: String?
+    var photoURL: String?
+    var date: TimeInterval
+}
+
+extension ChatObject {
+    func toModel() -> Chat {
+        .init(chatId: chatId,
+              userId: userId,
+              message: message,
+              photoURL: photoURL,
+              date: Date(timeIntervalSince1970: date))
+    }
+}
