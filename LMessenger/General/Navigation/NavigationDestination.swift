823979030f17209de80bd829258f4131be80b309
ChoiAnYong <dksdyd78@naver.com>
2024-01-04 22:41:12 +0900

    SearchBar를 이용한 UI 구성

--- LMessenger/General/Navigation/NavigationDestination.swift
+++ LMessenger/General/Navigation/NavigationDestination.swift
@@ -9,6 +9,6 @@ import Foundation
 
 enum NavigationDestination: Hashable {
     case chat(chatRoomId: String, myUserId: String, otherUserId: String )
-    case search
+    case search(userId: String)
 }
 
